386d676ef921bc0b5083ecfb3335b2729ef10a74
Ahmed-wael <you@example.com>
2015-02-16 22:43:45 +0200

    comments added

--- tryuser/db/schema.rb
+++ tryuser/db/schema.rb
@@ -13,6 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20150215201057) do
 
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password"
