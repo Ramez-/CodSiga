e3ba7b55ac041d6e89ec64ad6e1df0ff6921a436
Omar Saleh <omarsaleh0106@gmail.com>
2015-02-22 23:26:12 +0200

    fixed a few bugs

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -33,11 +33,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    @user.is_auth = false
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to posts_path , notice: 'User was successfully created. Please Sign in' }
+        format.json { render :show, status: :created, location: posts_path }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
